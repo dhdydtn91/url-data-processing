cc4a1f39d368a7821150cf1f9d1c99a76adb13a1
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-09 23:24:05 +0900

    test: 테스트 코드 추가 - 입력받은 출력단위에 따른 출력값을 만든다.

--- src/test/java/com/api/urldataprocessing/domain/processing/DataTest.java
+++ src/test/java/com/api/urldataprocessing/domain/processing/DataTest.java
@@ -17,8 +17,8 @@ class DataTest {
         English english = data.getEnglish();
         Numbers numbers = data.getNumbers();
 
-        assertThat(english.output()).isEqualTo("AAceeHHLLMMsTTW");
-        assertThat(numbers.output()).isEqualTo("222334");
+        assertThat(english.output()).isEqualTo("HTMLWeeAAscHTML");
+        assertThat(numbers.output()).isEqualTo("223234");
     }
 
     @DisplayName("오름차순으로 정렬한다.")
@@ -35,5 +35,17 @@ class DataTest {
         assertThat(numbers.output()).isEqualTo("222334");
     }
 
+    @DisplayName("입력받은 출력단위에 따른 출력값을 만든다.")
+    @Test
+    void changeOutputUnit() {
+        String html = "<HTML>W@e2eAAsc232    34<HTML>";
+        Data data = Data.of(html);
 
+        data.ascendingSort();
+        data.changeOutputUnit(4);
+        Output output = data.getOutput();
+        assertThat(output.getTotalStr()).isEqualTo("A2A2c2e3e3H4HLLMMsTTW");
+        assertThat(output.getQuotient()).isEqualTo("A2A2c2e3e3H4HLLMMsTT");
+        assertThat(output.getRemainder()).isEqualTo("W");
+    }
 }
