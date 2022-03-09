ee662230229748efd6b0b8cc4579f5c67759182b
Oh yong Soo <dhdydtn2019@gmail.com>
2022-03-09 17:44:46 +0900

    refactor : 예외처리에따른 리팩토링

--- src/main/java/com/api/urldataprocessing/common/exception/FailedScrapingHtmlException.java
+++
@@ -1,7 +0,0 @@
-package com.api.urldataprocessing.common.exception;
-
-public class FailedScrapingHtmlException extends RuntimeException {
-    public FailedScrapingHtmlException(String message) {
-        super(message);
-    }
-}
